203cc816a2778c766d2f73ce93124f5cc67c0ac1
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-24 05:59:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,15 @@ def monopoly_with_third_tier
   monopoly = {:railroads => {
     :pieces => 4,
     :rent_in_dollars => {
-      :one_piece_owned
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,      
+      :three_piece_owned => 100,
+      :four_piece_owned => 200     
     },
-    :names => {}
+    :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad
+    }
   }
 
 def monopoly_with_fourth_tier
